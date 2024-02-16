139d01ea029446411dd5f88ba1e1904bbcf56f48
DRI <dristimaharjan7@gmail.com>
2024-02-16 15:58:00 +0545

    alert and contact form

--- Day2/script.js
+++ Day2/script.js
@@ -1,4 +1,22 @@
-function day2() { 
-    alert ("link");
+function onSubmit() { 
+    // alert ("link");
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const number = document.getElementById("number").value;
+
     
+    localStorage.setItem("firstName", firstName);
+    localStorage.setItem("lastName", lastName);
+    localStorage.setItem("email",email);
+    localStorage.setItem("number", number);
+
+ /* localStorage.getItem("firstName");
+    localStorage.getItem("lastName");
+    localStorage.getItem("email");
+    localStorage.getItem("number");
+    console.log("Submit");*/
+
+
 }
+
